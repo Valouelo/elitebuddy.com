69735e81c7e2611d1282e6e353efb14ca27d09f6
Laurent Liquidato <liquidato.laurent@gmail.com>
2024-06-11 14:58:51 +0200

    seed_file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-User.create!(user_id: 1, email: "lolo@gmail.com", password: "valentine")
+User.destroy_all
+Buddy.destroy_all
+user = User.create!(email: "lolo@gmail.com", password: "valentine")
+user2 = User.create!(email: "valou@gmail.com", password: "valentine")
 
-Buddy.create!(name: "Cyril", description: "organisateur de week-end!", options: "Developpeur Ruby hors pair", price_per_day: 3000)
-Buddy.create!(name: "Tom", description: "videur de vin!", options: "Developpeur de pintes", price_per_day: 38)
-Buddy.create!(name: "Jeremy", description: "Abdos en 6 packs", options: "Astronautes le week-end", price_per_day: 563)
+Buddy.create!(user_id: user.id, name: "Cyril", description: "organisateur de week-end!", options: "Developpeur Ruby hors pair", price_per_day: 3000)
+Buddy.create!(user_id: user2.id, name: "Tom", description: "videur de vin!", options: "Developpeur de pintes", price_per_day: 38)
+Buddy.create!(user_id: user.id, name: "Jeremy", description: "Abdos en 6 packs", options: "Astronaute le week-end", price_per_day: 563)
