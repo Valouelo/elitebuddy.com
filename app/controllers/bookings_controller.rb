454e604b9dd1c4a26c916608e93f0e1e5d140d0e
jeremy ziani <jeremy.ziani@gmail.com>
2024-06-11 14:07:04 +0200

    new view

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,26 @@
+class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :user_id, :buddy_id)
+  end
+
+end
