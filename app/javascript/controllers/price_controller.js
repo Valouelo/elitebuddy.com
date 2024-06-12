abaec65b3fbabd0e8fba123b2bdbb40abdbd1714
jeremy ziani <jeremy.ziani@gmail.com>
2024-06-12 15:16:20 +0200

    price booking done

--- app/javascript/controllers/price_controller.js
+++ app/javascript/controllers/price_controller.js
@@ -2,12 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="price"
 export default class extends Controller {
-  connect() {
-
-  }
+  static targets = ["startTime", "endTime", "priceInput"]
+  static values = { price: Number }
 
   calculatePrice() {
+    const startTime = new Date(this.startTimeTarget.value)
+    const endTime = new Date(this.endTimeTarget.value)
+// 
+    const millDiff = endTime - startTime
+    const daysDiff = millDiff / (1000 * 3600 * 24)
 
-    
+    const pricePerDay = this.priceValue
+    const totalPrice = pricePerDay * daysDiff
+
+    this.priceInputTarget.value = totalPrice
   }
 }
