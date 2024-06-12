74935cb774d71a767458f2062055f45f8b8d5b92
valentine guy <valentineguy@yahoo.fr>
2024-06-12 10:40:23 +0200

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :buddies
+  has_one_attached :photo
 end
