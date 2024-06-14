10949a50c2a09bd3dd6dc16429c84156038dbd60
valentine guy <valentineguy@yahoo.fr>
2024-06-14 10:21:05 +0200

    back

---
+++ app/javascript/controllers/filter_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="filter"
+export default class extends Controller {
+  static targets = [ "card" ]
+
+  filter(event) {
+
+    const filterCategory = event.target.innerHTML
+    const cards = this.cardTargets
+    cards.forEach(function (card) {
+      const cardCategory = card.getAttribute('data-category')
+      if (cardCategory !== filterCategory) {
+        card.style.display = 'none';
+      } else {
+        card.style.display = '';
+      }
+
+    })
+
+  }
+
+  reset() {
+    const cards = this.cardTargets
+    cards.forEach(function (card) {
+      const cardCategory = card.getAttribute('data-category')
+        card.style.display = '';
+    })
+  }
+}
